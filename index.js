dd958ca502646f46140437ada443b82053d68283
rsingh0825 <74266936+rsingh0825@users.noreply.github.com>
2020-11-16 11:15:14 +0530

    Add Files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+setInterval(() => {
+  d=new Date();
+  hr_time=d.getHours();
+  min_time=d.getMinutes();
+  sec_time=d.getSeconds();
+  hr_rotation=(30*hr_time)+(min_time/2);
+  min_rotation=6*min_time;
+  sec_rotation=6*sec_time;
+  hour.style.transform=`rotate(${hr_rotation}deg)`;
+  minute.style.transform=`rotate(${min_rotation}deg)`;
+  second.style.transform=`rotate(${sec_rotation}deg)`;
+}, 1000);
